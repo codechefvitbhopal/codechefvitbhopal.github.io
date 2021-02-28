05b4275a88abc47ba9f81d0ecf4da61cd726f831
Ankit Sagar <62764278+woinbo@users.noreply.github.com>
2021-02-28 16:40:34 +0530

    res

--- lib/widgets/about_us.dart
+++ lib/widgets/about_us.dart
@@ -12,57 +12,61 @@ class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveWidget.isMobileScreen(context)
-        ? Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "About Us",
-                style: TextStyle(
-                  fontSize: 36,
-                  fontFamily: "Segoe UI",
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: ResponsiveWidget.isMobileScreen(context) ? 200 : 460,
-                child: AutoSizeText(
-                  "CodeChef VIT Bhopal Chapter is a programming club run and maintained by the official chapter leaders and mentored by CodeChef. The chapter drives to make learning competitive programming accessible to students",
-                  textAlign: TextAlign.justify,
+        ? Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "About Us",
                   style: TextStyle(
-                    fontSize: ResponsiveWidget.isMobileScreen(context) ? 4 : 22,
+                    fontSize: 36,
                     fontFamily: "Segoe UI",
-                    fontWeight: FontWeight.w100,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Our Vision",
-                style: TextStyle(
-                  fontSize: 36,
-                  fontFamily: "Segoe UI",
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: ResponsiveWidget.isMobileScreen(context) ? 200 : 500,
-                child: AutoSizeText(
-                  "CodeChef VIT Bhopal Chapter believes in providing everyone with a community of dedicated programmers to build a robust Competitive Programming culture on our campus. The chapter aims to make the college's coding culture the very best culture. The community emphasis to smooth your coding skills, data structures and algorithmic knowledge.",
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: ResponsiveWidget.isMobileScreen(context) ? 4 : 22,
-                    fontFamily: "Segoe UI",
-                    fontWeight: FontWeight.w100,
+                Container(
+                  width: ResponsiveWidget.isMobileScreen(context) ? 200 : 460,
+                  child: AutoSizeText(
+                    "CodeChef VIT Bhopal Chapter is a programming club run and maintained by the official chapter leaders and mentored by CodeChef. The chapter drives to make learning competitive programming accessible to students",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize:
+                          ResponsiveWidget.isMobileScreen(context) ? 4 : 22,
+                      fontFamily: "Segoe UI",
+                      fontWeight: FontWeight.w100,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Our Vision",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontFamily: "Segoe UI",
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: ResponsiveWidget.isMobileScreen(context) ? 200 : 500,
+                  child: AutoSizeText(
+                    "CodeChef VIT Bhopal Chapter believes in providing everyone with a community of dedicated programmers to build a robust Competitive Programming culture on our campus. The chapter aims to make the college's coding culture the very best culture. The community emphasis to smooth your coding skills, data structures and algorithmic knowledge.",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize:
+                          ResponsiveWidget.isMobileScreen(context) ? 4 : 22,
+                      fontFamily: "Segoe UI",
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           )
         : Container(
             height: 700,
@@ -75,7 +79,7 @@ class AboutUs extends StatelessWidget {
                 fit: BoxFit.contain,
               ),
             ),
-            child: Column(
+            child: Stack(
               children: [
                 Positioned(
                   top: 60,
