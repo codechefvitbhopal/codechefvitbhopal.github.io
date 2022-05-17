f7066a78cdb9e828141d0e2c4f6ca4a2f6a538ea
Anmol Verma <anmolverma102002@gmail.com>
2022-05-17 14:57:54 +0530

    Added react reveal at Home Page

--- components/Home/header.jsx
+++ components/Home/header.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import Jello from 'react-reveal/Jello';
+import Bounce from 'react-reveal/Bounce';
 import {Header,HeaderImg,Signup} from './styles'
 import styled from 'styled-components'
 export default function header() {
     return (
         <Header style={{width:'100%',height:'fit-content',display:'flex',flexDirection:'column',alignItems:'center'}}>
                <HeaderImg>
-                   {/* <Jello> */}
                     <Img>
                     <img src="/imageasset/heading.png" width="100%" height="auto"></img>
                     </Img>
-                   {/* </Jello> */}
-                   <Signup><a href="https://www.codechef.com/college-chapter/member" target="_blank">SIGN UP</a></Signup>
+                        <Signup><a href="https://www.codechef.com/college-chapter/member" target="_blank">SIGN UP</a></Signup>
                </HeaderImg>
         </Header>
     );
